a5c36c9745d727a79d7bef1f80d1fa05b18cb6f2
growbook91 <70497021+growbook91@users.noreply.github.com>
2022-07-06 15:07:20 +0900

    This is an example of multi nav

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,296 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: App(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+enum TabItem { red, green, blue }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+Map<TabItem, String> tabName = {
+  TabItem.red: 'red',
+  TabItem.green: 'green',
+  TabItem.blue: 'blue',
+};
 
-  final String title;
+Map<TabItem, MaterialColor> activeTabColor = {
+  TabItem.red: Colors.red,
+  TabItem.green: Colors.green,
+  TabItem.blue: Colors.blue,
+};
 
+class App extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => AppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class AppState extends State<App> {
+  var _currentTab = TabItem.red;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _selectTab(TabItem tabItem) {
+    setState(() => _currentTab = tabItem);
   }
+
+  final _navigatorKeys = {
+    TabItem.red: GlobalKey<NavigatorState>(),
+    TabItem.green: GlobalKey<NavigatorState>(),
+    TabItem.blue: GlobalKey<NavigatorState>(),
+  };
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: Stack(children: <Widget>[
+        _buildOffstageNavigator(TabItem.red),
+        _buildOffstageNavigator(TabItem.green),
+        _buildOffstageNavigator(TabItem.blue),
+      ]),
+      bottomNavigationBar: BottomNavigation(
+        currentTab: _currentTab,
+        onSelectTab: _selectTab,
+      ),
+    );
+  }
+
+  Widget _buildOffstageNavigator(TabItem tabItem) {
+    return Offstage(
+      offstage: _currentTab != tabItem,
+      child: TabNavigator(
+        navigatorKey: _navigatorKeys[tabItem],
+        tabItem: tabItem,
+      ),
+    );
+  }
+  // Widget _buildBody() {
+  //   return Container(
+  //       color: activeTabColor[TabItem.red],
+  //       alignment: Alignment.center,
+  //       child: TextButton(
+  //         child: Text(
+  //           'PUSH',
+  //           style: TextStyle(fontSize: 32.0, color: Colors.white),
+  //         ),
+  //         onPressed: _push,
+  //       ));
+  // }
+
+  // void _push() {
+  //   Navigator.of(context).push(MaterialPageRoute(
+  //     // we'll look at ColorDetailPage later
+  //     builder: (context) => ColorDetailPage(
+  //       color: activeTabColor[TabItem.red],
+  //       title: tabName[TabItem.red],
+  //     ),
+  //   ));
+  // }
+}
+
+class BottomNavigation extends StatelessWidget {
+  BottomNavigation({required this.currentTab, required this.onSelectTab});
+  final TabItem currentTab;
+  final ValueChanged<TabItem> onSelectTab;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: [
+        _buildItem(TabItem.red),
+        _buildItem(TabItem.green),
+        _buildItem(TabItem.blue),
+      ],
+      onTap: (index) => onSelectTab(
+        TabItem.values[index],
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _buildItem(TabItem tabItem) {
+    return BottomNavigationBarItem(
+      icon: Icon(
+        Icons.layers,
+        color: _colorTabMatching(tabItem),
+      ),
+      label: tabName[tabItem],
+    );
+  }
+
+  MaterialColor? _colorTabMatching(TabItem item) {
+    return currentTab == item ? activeTabColor[item] : Colors.grey;
+  }
+}
+
+class TabNavigatorRoutes {
+  static const String root = '/';
+  static const String detail = '/detail';
+}
+
+// 2
+class TabNavigator extends StatelessWidget {
+  TabNavigator({required this.navigatorKey, required this.tabItem});
+  final GlobalKey<NavigatorState>? navigatorKey;
+  final TabItem tabItem;
+
+  // 3
+  Map<String, WidgetBuilder> _routeBuilders(BuildContext context,
+      {int materialIndex: 500}) {
+    return {
+      TabNavigatorRoutes.root: (context) => ColorsListPage(
+            color: activeTabColor[tabItem],
+            title: tabName[tabItem],
+            onPush: (materialIndex) =>
+                _push(context, materialIndex: materialIndex),
+          ),
+      TabNavigatorRoutes.detail: (context) => ColorDetailPage(
+            color: activeTabColor[tabItem],
+            title: tabName[tabItem],
+            materialIndex: materialIndex,
+          ),
+    };
+  }
+
+  // 4
+  @override
+  Widget build(BuildContext context) {
+    final routeBuilders = _routeBuilders(context);
+    return Navigator(
+      key: navigatorKey,
+      initialRoute: TabNavigatorRoutes.root,
+      onGenerateRoute: (routeSettings) {
+        return MaterialPageRoute(
+          //왜 null?
+          builder: (context) {
+            if (routeSettings.name == '/detail') {
+              return ColorDetailPage(
+                color: activeTabColor[tabItem],
+                title: tabName[tabItem],
+                materialIndex: 500,
+              );
+            } else {
+              return ColorsListPage(
+                color: activeTabColor[tabItem],
+                title: tabName[tabItem],
+                onPush: (materialIndex) =>
+                    _push(context, materialIndex: materialIndex),
+              );
+            }
+          },
+        );
+      },
+    );
+  }
+
+  // 5
+  void _push(BuildContext context, {int materialIndex: 500}) {
+    var routeBuilders = _routeBuilders(context, materialIndex: materialIndex);
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          if (TabNavigatorRoutes.detail == '/detail') {
+            return ColorDetailPage(
+                color: activeTabColor[tabItem],
+                title: tabName[tabItem],
+                materialIndex: 500,
+              );
+          } else {
+            return ColorsListPage(
+              color: activeTabColor[tabItem],
+              title: tabName[tabItem],
+              onPush: (materialIndex) =>
+                  _push(context, materialIndex: materialIndex),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class ColorsListPage extends StatelessWidget {
+  ColorsListPage(
+      {required this.color, required this.title, required this.onPush});
+  final MaterialColor? color;
+  final String? title;
+  final ValueChanged<int> onPush;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            title!,
+          ),
+          backgroundColor: color,
+        ),
+        body: Container(
+          color: Colors.white,
+          child: _buildList(),
+        ));
+  }
+
+  final List<int> materialIndices = [
+    900,
+    800,
+    700,
+    600,
+    500,
+    400,
+    300,
+    200,
+    100,
+    50
+  ];
+
+  Widget _buildList() {
+    return ListView.builder(
+        itemCount: materialIndices.length,
+        itemBuilder: (BuildContext content, int index) {
+          int materialIndex = materialIndices[index];
+          return Container(
+            color: color![materialIndex],
+            child: ListTile(
+              title: Text('$materialIndex', style: TextStyle(fontSize: 24.0)),
+              trailing: Icon(Icons.chevron_right),
+              onTap: () => onPush(materialIndex),
+            ),
+          );
+        });
+  }
+}
+
+class ColorDetailPage extends StatelessWidget {
+  ColorDetailPage(
+      {required this.color, required this.title, this.materialIndex: 500});
+  final MaterialColor? color;
+  final String? title;
+  final int materialIndex;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        backgroundColor: color,
+        title: Text(
+          '$title[$materialIndex]',
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Container(
+        color: color![materialIndex],
+      ),
     );
   }
 }
